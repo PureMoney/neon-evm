8fa6aee1d18de96363a816fd3c58e76177adf1ec
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-02-22 20:55:30 +0500

    Pass NEON_EVM_REVISION, NEON_EVM_BRANVH, NEON_EVM_REPO

--- .buildkite/steps/trigger-proxy-step.sh
+++ .buildkite/steps/trigger-proxy-step.sh
@@ -11,5 +11,8 @@ cat << EOF
           EVM_LOADER_BRANCH: "${BUILDKITE_BRANCH}"
           SOLANA_REVISION: "v1.8.12-testnet"
           EVM_LOADER_FULL_TEST_SUITE: $(buildkite-agent meta-data get "full_test_suite" --default "false")
+          NEON_EVM_REVISION: "${BUILDKITE_COMMIT}"
+          NEON_EVM_BRANCH: "${BUILDKITE_BRANCH}"
+          NEON_EVM_REPO: "${NEON_EVM_REPO}"
 EOF
 
