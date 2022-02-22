a155f9f6d649730a65a509fda257705d76b85394
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2022-02-22 16:18:28 +0500

    add extra env

--- .buildkite/steps/trigger-proxy-pipeline.sh
+++ .buildkite/steps/trigger-proxy-pipeline.sh
@@ -3,12 +3,13 @@
 cat << EOF
   - label: ":docker: trigger python logged groups pipeline"
     trigger: "rozhkovdmitrii-sanbox-pipeline"
-
+    key: "trigger-proxy"
     build:
       branch: "play-buildkite"
       env:
-          EVM_LOADER_REVISION: "$${BUILDKITE_COMMIT}"
-          EVM_LOADER_BRANCH: "$${EVM_LOADER_BRANCH}"
+          EVM_LOADER_REVISION: "${BUILDKITE_COMMIT}"
+          EVM_LOADER_BRANCH: "${EVM_LOADER_BRANCH}"
+          GGBB: "${GGBB}"
           SOLANA_REVISION: "v1.8.12-testnet"
           EVM_LOADER_FULL_TEST_SUITE: $(buildkite-agent meta-data get "full_test_suite" --default "false")
 EOF
